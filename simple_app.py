eadac7818408f5dfd53e3003051a53f9a69db358
Amandeep Singh <144270371+Amandeep-Singh07@users.noreply.github.com>
2025-04-03 20:32:47 +0530

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -1,11 +1,14 @@
-import streamlit as st
+import streamlit as st  # Import Streamlit library for building the web app
 
+# Define the main function to run the Streamlit app
 def main():
-    st.title('Simple Test App')
-    st.write('Testing if Streamlit works')
+    st.title('Simple Test App')  # Set the title of the Streamlit app
+    st.write('Testing if Streamlit works')  # Display a message on the page
     
+    # Create a button that, when clicked, displays a message
     if st.button('Click me'):
-        st.write('Button clicked!')
+        st.write('Button clicked!')  # Display this message when the button is clicked
 
+# Run the main function when the script is executedd
 if __name__ == '__main__':
     main()
